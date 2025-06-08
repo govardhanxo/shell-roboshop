8cf0fdd2327e2e3cbc99a041d2031ba194d88e6a
govardhan <govardhanxo@gmail.com>
2025-06-09 00:21:50 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -9,8 +9,8 @@ DOMAIN_NAME="govardhanxo.site"
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0437385750f10d4ef --tag-specificaions "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId --output text)
-    if [ $instance != frontend]
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0437385750f10d4ef --tag-specificaions "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
