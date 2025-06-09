b9e9e9e9ae48af24ceb434f9193e9530f56e4674
govardhan <govardhanxo@gmail.com>
2025-06-10 00:48:19 +0530

    roboshop1

--- catalogue.sh
+++ catalogue.sh
@@ -57,7 +57,9 @@ VALIDATE $? "Creating app directory"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading Catalogue"
 
-cd /app 
+
+rm -rf /app/*
+cd /app
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "Unzipping Catalogue"
 
