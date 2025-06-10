27c6cd824c539950ce76fc95bff89de5968cbaec
govardhan <govardhanxo@gmail.com>
2025-06-10 21:07:39 +0530

    roboshop

--- mysql.sh
+++ mysql.sh
@@ -54,5 +54,5 @@ VALIDATE $? "Setting MySQL root password"
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script execution completed successfully, $Y time taken $TOTAL_TIME seconds $N" &>>$LOG_FILE
+echo -e "Script execution completed successfully, $Y time taken $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
 
