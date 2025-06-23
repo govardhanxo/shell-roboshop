fa5a7ef3344c11e182e4b5db8f0b23c10da97fc8
govardhan <govardhanxo@gmail.com>
2025-06-23 12:49:46 +0530

    ansible-roles

--- roboshop.sh
+++ roboshop.sh
@@ -3,8 +3,8 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-085ea8786c9d11b9d" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
-DOMAIN_NAME="daws84s.site" # replace with your domain
+ZONE_ID="Z027737329YBJVEFGKUR8" # replace with your ZONE ID
+DOMAIN_NAME="govardhanxo.site" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
